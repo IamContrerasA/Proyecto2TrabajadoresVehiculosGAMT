e14e699de1f2c57ae7d87685ac52cf5fceb0a891
Iam Contreras Alcazar <iamcontrerasalcazar@gmail.com>
2021-01-16 17:01:14 -0500

    update design general programations with camera

--- static/js/check_device_camera.js
+++ static/js/check_device_camera.js
@@ -1,3 +1,14 @@
+var scroll_y, scroll_x;
+  window.addEventListener("scroll", function(event) {  
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      scroll_y = 0; 
+      scroll_x = 0; 
+    }else{
+      scroll_y = this.scrollY; 
+      scroll_x = this.scrollX; 
+    }   
+  });
+
 var final_width = 640;
 var final_height = 480;
 var camera_width = 640;
